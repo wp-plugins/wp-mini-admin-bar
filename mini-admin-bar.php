3d7086b9aedf5229d9de8dbc39cc42f7ca36531d
mag_oberon <mag_oberon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-03 04:56:32 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/wp-mini-admin-bar/trunk@1058865 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mini-admin-bar.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: WP Mini Admin Bar
+Plugin URI: http://devstech.com/
+Description: Makes the admin bar a small button on the left and expands on hover.
+Author: Ataul Ghani
+Version: 1.0
+Author URI: http://devstech.com/
+*/
+
+add_action('get_header', 'wp_mini_head');
+function wp_mini_head() { remove_action('wp_head', '_admin_bar_bump_cb'); }
+ 
+function wp_mini_admin() {
+        if ( is_user_logged_in() ) {
+        ?>
+        <style type="text/css">
+            #wpadminbar {
+                width: 47px;
+                min-width: auto;
+                overflow: hidden;
+                -webkit-transition: .4s width;
+                -webkit-transition-delay: 1s;
+                -moz-transition: .4s width;
+                -moz-transition-delay: 1s;
+                -o-transition: .4s width;
+                -o-transition-delay: 1s;
+                -ms-transition: .4s width;
+                -ms-transition-delay: 1s;
+                transition: .4s width;
+                transition-delay: 1s;
+            }
+            
+            #wpadminbar:hover {
+                width: 100%;
+                overflow: visible;
+                -webkit-transition-delay: 0;
+                -moz-transition-delay: 0;
+                -o-transition-delay: 0;
+                -ms-transition-delay: 0;
+                transition-delay: 0;
+            }
+        </style>
+        <?php }
+}
+add_action('wp_head', 'wp_mini_admin');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== WP Mini Admin Bar ===
+Contributors: mag_oberon
+Tags: admin, mini bar, admin bar, wp admin, wp admin bar, small admin bar, small bar, plugin
+Requires at least: 2.8
+Tested up to: 4.0
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Makes the admin bar a small button on the left and expands on hover.
+
+== Description ==
+
+Do you want to make your wp admin bar more smaller? WP Mini Admin Bar plugin will help to makes the admin bar a small button on the left and expands on hover.
+
+Just simply install this plugin and it will be automatically effect on your admin bar.
+
+
+== Installation ==
+
+1. Upload the 'wp-mini-admin-bar' folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. You’re done!
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+== Changelog ==
+
+== Upgrade Notice ==
+
